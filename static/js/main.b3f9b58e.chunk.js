54ebfb600720ab9f0c3a52c6051b6e413ed5a3dc
Nathan Endow <35786272+nendow02@users.noreply.github.com>
2021-11-25 22:11:48 -0800

    Updates

--- static/js/main.b3f9b58e.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonppersonal=this.webpackJsonppersonal||[]).push([[0],{11:function(e,n,t){"use strict";t.r(n);var c=t(1),s=t.n(c),r=t(3),o=t.n(r),i=(t(8),t(9),t(0));var a=function(){return Object(i.jsx)("div",{className:"App",children:"Test"})};o.a.render(Object(i.jsx)(s.a.StrictMode,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))},8:function(e,n,t){},9:function(e,n,t){}},[[11,1,2]]]);
-//# sourceMappingURL=main.b3f9b58e.chunk.js.map
